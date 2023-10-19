7deecee0d8d893d15f5e59246191fa95313c3bd0
RuanCostaDev <ruanrrc@hotmail.com>
2023-10-19 09:55:59 -0300

    first commit

---
+++ registro-de-estudantes.py
@@ -0,0 +1,11 @@
+while(True):
+    print("-\n| Sistema de Gerenciamento de Registro de Estudantes |\n-\n" +
+          "1. Adicionar Registro de Estudante\n"+
+          "2. Exibir Registro de Estudantes\n" +
+          "3. Procurar por um Estudante\n" +
+          "4. Calcular Média das Notas\n" +
+          "5. Salvar Registros em Arquivo\n" +
+          "6. Carregar Registros de Arquivo\n" +
+          "7. Sair\n---\n")
+    
+    opcao = input("Digite sua escolha (1-7): ")
